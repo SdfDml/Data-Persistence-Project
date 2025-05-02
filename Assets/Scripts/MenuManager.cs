24dc4864e813a7fc979809887ff606fb57bb7c22
SdfDml <damlakumser@gmail.com>
2025-05-02 09:46:17 +0300

    Main menu scene added
    Main menu scene added and its start and Quit buttons are activated.
    
    Switch between scenes provided.
    
    Back to main menu option by escape key was added to game over screen.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        EditorApplication.ExitPlaymode();
+    }
+}
